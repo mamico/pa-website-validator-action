79fc35194850360da428e96e292cc465cd71bc43
Mauro Amico <mauro.amico@gmail.com>
2024-06-01 23:03:28 +0200

    add script

---
+++ scripts/process_audit.py
@@ -0,0 +1,32 @@
+
+import json
+# import datetime
+from glob import glob
+import traceback
+
+
+def parse_files():
+    files = glob("*/report.json")
+    data = []
+    for file in files:
+        try:
+            audit = json.load(open(file))
+            item = {
+                'report': {
+                    'date': audit['fetchTime'],
+                    'url': f"./{file.replace('.json', '.html')}"
+                },
+                'site': audit['finalUrl'],
+                'performance': int((audit['categories']['performance']['score'] or 0) * 100),
+                'modelComplianceInformation': int((audit['categories']['modelComplianceInformation']['score'] or 0) * 100),
+                'reccomandationsAndAdditionalTests': int((audit['categories']['reccomandationsAndAdditionalTests']['score'] or 0) * 100),
+                'additionalTests': int((audit['categories']['additionalTests']['score'] or 0) * 100)
+            }
+            data.append(item)
+        except:
+            print(f"Error parsing {file}")
+            print(traceback.format_exc())
+    return data
+
+data = parse_files()
+print(json.dumps(data, indent=2))
